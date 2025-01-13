fd213f94063a54ec31532ea1e91c945b029ccac9
Alhasan Al-Qaysi <aa033368@tite.amiedu-ict.net>
2025-01-13 13:36:15 +0200

    Remove first task

--- todo.py
+++ todo.py
@@ -1,1 +1,10 @@
-dev reomve_task()
+tasks = []  # List to store tasks
+
+def remove_task(task_index):
+    try:
+        removed_task = tasks.pop(task_index)
+        print(f"Removed task: {removed_task}")
+    except IndexError:
+        print("Invalid task index")
+    except:
+        print("Failed to remove task")
