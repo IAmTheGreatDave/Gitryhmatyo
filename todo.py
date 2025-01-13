a907c6d5ed3f35d16e77ae710a1650f0d4e60b12
mikkis24 <miksu.nykanen@gmail.com>
2025-01-13 13:56:21 +0200

    Add view_tasks function (#3)

--- todo.py
+++ todo.py
@@ -1,7 +1,13 @@
+def view_tasks( lista ):
+    if lista:
+        for idx,data in enumerate(lista):
+            print(f'( {idx} ) {lista[idx]}')
+    else:
+        print('Congrats. No tasks.')
+
 
 def add_task(lista):
     task = input("Give task: ")
     lista.append(task)
 
-add_task()
 
